4688524ab58a5b1fa5739636c7268239a5a16933
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-01-07 08:37:37 +0100

    Fix register

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
@@ -16,7 +15,7 @@ class SessionController extends Controller
     {
         $attributes = request()->validate([
             'email'=>'required|email',
-            'password'=>'required' 
+            'password' => 'required'
         ]);
 
         if(Auth::attempt($attributes))
@@ -27,7 +26,7 @@ class SessionController extends Controller
 
         return back()->withErrors(['msgError' => 'These credentials do not match our records.']);
     }
-    
+
     public function destroy()
     {
 
