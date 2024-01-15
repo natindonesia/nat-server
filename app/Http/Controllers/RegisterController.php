d2e9da628e5d51b244da5e4a1f87575429a248ad
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-01-15 09:12:12 +0100

    fix: Redirect after register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -35,6 +35,6 @@ class RegisterController extends Controller
 
         session()->flash('success', 'Your account has been created.');
         Auth::login($insertUser);
-        return view('dashboards/default');
+        return redirect('/');
     }
 }
