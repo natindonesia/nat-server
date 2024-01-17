24a7a620f20801ce5b1548e02ba72cd24923af81
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-01-17 12:49:58 +0100

    fix: PDF hopefully

--- app/Exports/SensorDataExport.php
+++ app/Exports/SensorDataExport.php
@@ -5,13 +5,19 @@ namespace App\Exports;
 use App\Http\Controllers\WaterpoolController;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\RegistersEventListeners;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 
 
-class SensorDataExport implements FromCollection, ShouldAutoSize
+class SensorDataExport implements FromCollection, ShouldAutoSize, WithEvents
 {
 
+    use Exportable, RegistersEventListeners;
     protected $deviceName;
 
 
@@ -25,8 +31,8 @@ class SensorDataExport implements FromCollection, ShouldAutoSize
      */
     public function collection()
     {
-        $states = Cache::remember(SensorDataExport::class . 'states', 60 * 15, function () {
-            return WaterpoolController::getStates($this->deviceName, 600);
+        $states = Cache::remember(SensorDataExport::class . 'states2', 60 * 15, function () {
+            return WaterpoolController::getStates($this->deviceName, 200);
         });
         $collection = new Collection();
         if (empty($states)) return $collection;
@@ -44,6 +50,9 @@ class SensorDataExport implements FromCollection, ShouldAutoSize
         foreach ($states as $state) {
             $values = [];
             foreach ($state as $key => $value) {
+                if ($value === 'unavailable') {
+                    $value = '?';
+                }
                 if ($key === 'timestamp') {
                     $value = date('Y-m-d H:i:s', $value);
                     $values[] = $value;
@@ -57,4 +66,10 @@ class SensorDataExport implements FromCollection, ShouldAutoSize
     }
 
 
+    public static function afterSheet(AfterSheet $event)
+    {
+
+        $event->getSheet()->getDelegate()->getStyle('A1:Z1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $event->getSheet()->getDelegate()->getStyle('A1:Z1')->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+    }
 }
