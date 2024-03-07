23a47a9e8b325ab2e7185841fdd8b962fd9dab58
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-03-07 03:46:04 +0100

    feat: More caching

--- app/View/Components/DetailTable.php
+++ app/View/Components/DetailTable.php
@@ -3,6 +3,7 @@
 namespace App\View\Components;
 
 use App\Models\Pool\StateLog;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\Component;
 
 class DetailTable extends Component
@@ -26,10 +27,15 @@ class DetailTable extends Component
      */
     public function render()
     {
-        $status = StateLog::where('device', $this->deviceName)->orderBy('created_at', 'desc')
-            ->limit(15)
-            ->get()
-            ->toArray();
+
+        $status =
+            Cache::remember('status.' . $this->deviceName, config('cache.time'), function () {
+                return StateLog::where('device', $this->deviceName)->orderBy('created_at', 'desc')
+                    ->limit(15)
+                    ->get()
+                    ->toArray();
+            });
+
         return view('components.detail-table', compact('status'));
     }
 }
