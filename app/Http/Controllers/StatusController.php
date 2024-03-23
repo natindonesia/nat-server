766ec1c27a18712758989ffbd507d3d01e6100ab
RizkyMardiyansyah <rizkymardiyansyah23@gmail.com>
2024-03-23 15:13:08 +0700

    update param cl

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -76,7 +76,7 @@ class StatusController extends Controller
         ],
         [
             'sensor' => 'tds',
-            'min' => -2000,
+            'min' => 0,
             'max' => 699,
             'score' => 0.69
         ],
@@ -94,7 +94,7 @@ class StatusController extends Controller
         ],
         [
             'sensor' => 'cl',
-            'min' => -100,
+            'min' => -10,
             'max' => 0.9,
             'score' => 0.69
         ],
